289491a295c2cd4cfb35c693b5bde440c05afaf8
Serhii Varakuta <flood.code@gmail.com>
2022-09-19 10:10:01 +0300

    Added bridge page

---
+++ pages/bridge/index.tsx
@@ -0,0 +1,24 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+
+const Bridge: NextPage = () => {
+  return (
+    <div>
+      <Head>
+        <title>Bridge</title>
+        <meta name="description" content="Bridge" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+        Main
+      </main>
+
+      <footer>
+        Footer
+      </footer>
+    </div>
+  )
+}
+
+export default Bridge
