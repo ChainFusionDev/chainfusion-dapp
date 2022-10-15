11dad850a31b3ac0e809e9ba4cca81a39888bdb0
Serhii Varakuta <flood.code@gmail.com>
2022-10-16 00:29:23 +0300

    Fix CSS loading order issue

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,10 @@
+
 import 'bootstrap/dist/css/bootstrap.css'
-import '../styles/globals.css'
-import '../styles/responsive.css'
-import '../styles/fontawesome/css/all.min.css'
 import 'animate.css';
+import '@styles/fontawesome/css/all.css'
+import '@styles/inter.css';
+import '@styles/global.css'
+import '@styles/responsive.css'
 
 import type { AppProps } from 'next/app'
 import { useEffect } from 'react'
