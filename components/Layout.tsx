5dc132a72d4a0867ef38a2907fb88bb57c50c3d4
Serhii Varakuta <gitlab@varakuta.com>
2022-10-18 17:18:52 +0000

    BG-416 Added a footer

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -21,9 +21,57 @@ function Layout({ children, module, title, description }: LayoutProps) {
         <link rel="icon" href="/img/favicon.svg" sizes="any" type="image/svg+xml" />
       </Head>
 
-      <main>
+      <main className="d-flex flex-column h-100">
         <Navbar module={module} />
         {children}
+
+        <footer className="d-flex mt-auto align-items-center">
+          <div className="container">
+            <div className="row">
+              <div className="col-12 footer-block d-flex">
+                <div className="copyright d-flex justify-content-start align-items-center">
+                  ChainFusion © {new Date().getFullYear()}
+                </div>
+                <ul className="footer-link d-flex justify-content-between mr-auto">
+                  <li>
+                    <a href="#">Home</a>
+                  </li>
+                  <li>
+                    <a href="#">Blockchain explorer</a>
+                  </li>
+                  <li>
+                    <a href="#">Documentation</a>
+                  </li>
+                  <li>
+                    <a href="#">Data privacy</a>
+                  </li>
+                </ul>
+                <ul className="footer-social-icon d-flex ml-auto">
+                  <li>
+                    <a href="#" target="_blank">
+                      <img src="img/social/twitter.svg" alt="Twitter" />
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#" target="_blank">
+                      <img src="img/social/telegram.svg" alt="Telegram" />
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#" target="_blank">
+                      <img src="img/social/medium.svg" alt="Medium" />
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#" target="_blank">
+                      <img src="img/social/linkedin.svg" alt="Linkedin" />
+                    </a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </footer>
       </main>
     </div>
   );
