3ec4e09444982acd5c4efaf8305729988f9dad9b
Roman Polishchuk <roman.polishchuk@neatfusion.org>
2022-11-17 12:44:18 +0000

    Resolve BG-461 "Implement validator list loading on staking page"

--- components/Staking/StakingTable.tsx
+++ components/Staking/StakingTable.tsx
@@ -24,23 +24,58 @@ export const StakingItem = ({ rank, data }: StakingItemProps) => {
     return new URL(`/address/${address}`, nativeChain.explorer).href;
   };
 
+  const getShortValidator = (tx: string) => {
+    const visibleCharacters = 16;
+    return `${tx.substring(0, 2 + visibleCharacters)}...${tx.substring(tx.length - visibleCharacters)}`;
+  };
+
   return (
     <div className="staking-table-tr d-flex">
       <div className="rank pr-md-3">
-        <p className="d-block d-sm-block d-md-none">Rank:</p>
+        <p className="d-block d-sm-block d-md-none">Rank:</p>&nbsp;
         {rank}
       </div>
       <div className="wallet pl-md-4 flex-grow-1">
         <p className="d-block d-sm-block d-md-none">Validator:</p>
         <a href={getAddressUrl(data.validator)} target="_blank" rel="noreferrer">
-          {data.validator}
+          <span className="d-block d-sm-none">{getShortValidator(data.validator)}</span>
+          <span className="d-none d-sm-block">{data.validator}</span>
         </a>
-        <span className="copy-token-icon" data-toggle="tooltip" data-tip data-for="transaction-copy"></span>
+        <span
+          className="copy-token-icon d-none d-md-block"
+          data-toggle="tooltip"
+          data-tip
+          data-for="transaction-copy"
+        ></span>
       </div>
       <div className="amount pl-md-4 pr-md-6">
-        <p className="d-block d-sm-block d-md-none">Stake:</p>
+        <p className="d-block d-sm-block d-md-none">Stake:</p>&nbsp;
         {utils.formatEther(data.stake)}
         {` ${nativeChain.nativeCurrency.symbol}`}
+      </div>
+    </div>
+  );
+};
+
+export const SkeletonStakingItem = () => {
+  return (
+    <div className="staking-table-tr d-flex">
+      <div className="rank pr-md-3">
+        <div className="skeleton">
+          <div className="avatar-skeleton"></div>
+        </div>
+      </div>
+      <div className="wallet pl-md-4 flex-grow-1">
+        <div className="skeleton mt-2 mt-md-0 pr-md-4">
+          <div className="line-skeleton"></div>
+          <div className="line-skeleton"></div>
+        </div>
+      </div>
+      <div className="amount pl-md-4 pr-md-6 d-none d-md-block">
+        <div className="skeleton">
+          <div className="line-skeleton"></div>
+          <div className="line-skeleton"></div>
+        </div>
       </div>
     </div>
   );
