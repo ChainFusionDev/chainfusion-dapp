c5ddd3bc3754572d574ec1af9fa73b61c0d400d9
Serhii Varakuta <flood.code@gmail.com>
2022-12-16 03:36:54 +0200

    Implement TVl loading for chains

--- components/Liquidity/ChainLiquidityItem.tsx
+++ components/Liquidity/ChainLiquidityItem.tsx
@@ -1,13 +1,69 @@
+import { MockToken__factory } from '@chainfusion/chainfusion-contracts';
 import { getSupportedTokens } from '@src/config';
+import { useChainContext } from '@src/context/ChainContext';
 import { Chain } from '@src/types';
+import { BigNumber, utils } from 'ethers';
 import Link from 'next/link';
+import { useEffect, useState } from 'react';
 
 export interface ChainLiquidityItemProps {
   chain: Chain;
 }
 
 export const ChainLiquidityItem = ({ chain }: ChainLiquidityItemProps) => {
+  const [isLoading, setIsLoading] = useState(true);
+  const [tvl, setTVL] = useState(BigNumber.from(0));
+
+  const { networkContainer } = useChainContext();
+
   const tokens = getSupportedTokens().filter((token) => token.chains[chain.identifier] !== undefined);
+
+  useEffect(() => {
+    let pending = true;
+
+    if (networkContainer.size == 0) {
+      return;
+    }
+
+    const network = networkContainer.get(chain.identifier);
+    if (network?.contracts === undefined) {
+      return;
+    }
+
+    const liquidityPoolsAddress = network.contracts.liqidityPools.address;
+
+    const loadTVL = async () => {
+      let tvl: BigNumber = BigNumber.from(0);
+      const tokenTVLPromises: Promise<BigNumber>[] = [];
+      for (const token of tokens) {
+        const tokenAddress = token.chains[chain.identifier];
+        if (tokenAddress === undefined) {
+          continue;
+        }
+
+        const mockTokenFactory = new MockToken__factory(network.provider.getSigner());
+        const mockToken = mockTokenFactory.attach(tokenAddress);
+
+        tokenTVLPromises.push(mockToken.balanceOf(liquidityPoolsAddress));
+      }
+
+      for (const tvlPromise of tokenTVLPromises) {
+        tvl = tvl.add(await tvlPromise);
+      }
+
+      if (pending) {
+        setTVL(tvl.sub(tvl.mod(BigNumber.from(10).pow(18))));
+        setIsLoading(false);
+      }
+    };
+
+    loadTVL();
+
+    return () => {
+      pending = false;
+    };
+  }, [networkContainer, chain, tokens]);
+
   const tokenItems = tokens.slice(0, 3).map((token) => {
     return (
       <li key={token.identifier}>
@@ -30,7 +86,7 @@ export const ChainLiquidityItem = ({ chain }: ChainLiquidityItemProps) => {
             </div>
             <div className="liquidity-tvl">
               <span>TVL</span>
-              <div className="liquidity-sum">$10 000</div>
+              <div className="liquidity-sum">{isLoading ? 'Loading' : `$${utils.commify(utils.formatEther(tvl))}`}</div>
             </div>
           </div>
         </div>
